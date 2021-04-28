46922f191b05a6bf5d48aafff1ed1910f083de22
HristoDobrikov <generatorfox@gmail.com>
2021-04-28 12:32:46 +0300

    Добавени 2 информационни секции

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Home</h1>
+<div class="info-box">
+	<p class="ta-c">
+		Добре дошли в нашия уебсайт! Тук ще откриете интересни<br> забележителности, цветни кътчета, гостоприемни заведения<br> и интересна информация за общи
+	</p>
+</div>
+
+
+<div class="info-box-2">
+	<p class="ta-c">
+		Не стойте гладни, а опитайте специалитетите на нашите<br> ресторанти! Също така, не пропускайте чаша топко кафе в<br> уютните ни заведения!
+	</p>
+</div>
 </template>
+
+<style lang="scss" scoped>
+.info-box{
+	background-color: var(--clr-other-green);
+	color: white;
+	padding: 30px;
+    font-size: 21px;
+}
+	
+	.info-box-2{
+	background-color: var(--clr-other-green);
+	color: white;
+	padding: 30px;
+    font-size: 21px;
+
+}
+</style>
