281e2f80bfa613cb4ffd9a934eba265b1d0f751f
HristoDobrikov <generatorfox@gmail.com>
2021-04-29 12:00:20 +0300

    Auto stash before merge of "home-page" and "origin/home-page"

---
+++ frontend/src/components/home/RestaurantBox.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="first-box purvo">
+	a				
+</div>
+
+<div class="first-box">
+	a				
+</div>
+
+<div class="first-box">
+	a				
+</div>
+
+<div class="first-box">
+	a				
+</div>
+</template>
+
+<style>
+.first-box{
+    width: 14%;
+    height: 35vh;
+    background-color: red;
+    margin: 0% 4% 0% 4%;
+    position: relative;
+    top: 10vh;
+}
+.purvo{
+margin-left: 10%;
+}
+		
+</style>
