1cb13717d6228093438569d7a065a9c73aac509e
Zdravko Hristov <zdravkohristov0@gmail.com>
2021-05-01 13:18:23 +0300

    Добавени икони + добавен компонент

---
+++ frontend/src/components/others/InfoBox.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="info-box">
+        <div class="info-icon">
+            <i class="fas fa-info-circle"></i>
+        </div>
+        <p class="ta-c">
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.info-box {
+    background-color: var(--clr-other-green);
+    color: white;
+    padding: 30px;
+    font-size: 21px;
+    position: relative;
+    .info-icon {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+}
+</style>
