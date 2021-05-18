c4216a474ee5d97d335fce065745ba4a0be61dd4
lSlav <lllubo9120@gmail.com>
2021-05-19 01:56:32 +0300

    suzdadena purva sekciq

--- frontend/src/views/Tourism.vue
+++ frontend/src/views/Tourism.vue
@@ -1,3 +1,45 @@
 <template>
-    <h1>Tourism</h1>
+    <section class="flex m-y-3 tourism-container tourism-info">
+        <div class="img-container"><img src="https://lh5.googleusercontent.com/-Ns7D-GLVfSY/UPkVEa1qbiI/AAAAAAAASrM/pVefFDCUV0Q/s512/4678443.jpg" alt="obshtina"></div>
+        <div class="ta-c p-x-2 text">
+            <h2>Община Първомай</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur perspiciatis autem, cupiditate explicabo placeat quas repudiandae, ut, consequatur dolorum dolor provident maiores debitis temporibus velit? Aut excepturi in error adipisci sed, ratione, obcaecati accusantium cumque eveniet minima repudiandae odit dolorem, optio sit sunt accusamus officiis pariatur vel magnam doloribus eos.
+            </p>
+        </div>
+    </section>
 </template>
+<style lang="scss" scoped>
+.tourism-container{
+   margin-left: auto;
+    margin-right: auto;
+    max-width:1300px;
+    &>* {flex:1;}
+    h2 {
+    font-size:2rem;
+    color:var(--clr-other-green);
+    margin-bottom:20px;
+    }
+    .img-container {
+        transform: scale(0.8);
+        position: relative;
+        &::before {
+          content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: var(--clr-other-green);
+    z-index: 4;
+    top: -40px;
+    left: -40px;
+    }
+    img {
+    z-index: 5;
+    position: relative;
+    }
+    }
+    }
+    * {
+        letter-spacing: 2px;
+    }
+    
+</style>
