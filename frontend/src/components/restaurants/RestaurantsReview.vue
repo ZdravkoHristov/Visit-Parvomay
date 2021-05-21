59d7600fca03531d82af7a1f95ddbd96a7b69b5d
HristoDobrikov <generatorfox@gmail.com>
2021-05-21 14:31:52 +0300

    Първа секция почната

---
+++ frontend/src/components/restaurants/RestaurantsReview.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="box">
+        <div class="img">
+        <div class="star">
+            4
+        </div>
+        <div class="dark-overlay"></div>
+        </div>
+
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/mixins' as *;
+.star {
+    width: 50px;
+    height: 50px;
+    background-color: var(--clr-other-green);
+    clip-path: polygon(
+        50% 0%,
+        61% 35%,
+        98% 35%,
+        68% 57%,
+        79% 91%,
+        50% 70%,
+        21% 91%,
+        32% 57%,
+        2% 35%,
+        39% 35%
+    );
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    transform: translate(-50%, -50%) rotate(-16deg);
+    position: relative;
+    z-index: 1;
+}
+
+.img{
+    position:relative;
+    background-image: url("../../assets/parvomay/hiphop.jpeg");
+    height: 100%;
+    background-size: cover;
+
+}
+.dark-overlay {
+    @include overlay;
+}
+.box {
+    background-size: cover;
+    border: 1px solid white;
+    p {
+        color: white;
+        font-size: 23px;
+        letter-spacing: 2px;
+        position: absolute;
+        bottom: 10px;
+        left: 10px;
+    }
+}
+</style>
--- frontend/src/views/Restaurants.vue
+++ frontend/src/views/Restaurants.vue
@@ -2,23 +2,64 @@
 
 <InfoBox>Вижте най-популярните заведения сред<br> посетителите на сайта</InfoBox>
 <section class="box-holder">
-<restaurant-box></restaurant-box>
+	<div class="with-text">
+		<restaurantsReview class="hipnoza left"></restaurantsReview>
+		<p>adds</p>
+	</div>
+
+	<div class="with-text">
+		<restaurantsReview class="golqma hipnoza"></restaurantsReview>
+		<p>adds</p>
+	</div>
+	<div class="with-text">
+		<restaurantsReview class="hipnoza"></restaurantsReview>
+		<p>adds</p>
+	</div>
 </section>
 
 
 
 </template>
 <script>
-import RestaurantBox from "@/components/home/RestaurantBox";
+import RestaurantsReview from "@/components/restaurants/RestaurantsReview";
 import InfoBox from "@/components/others/InfoBox";
 export default {
-   components: { RestaurantBox, InfoBox }
+   components: { RestaurantsReview, InfoBox }
 };
 </script>
 <style lang="scss" scoped>
 
 
+.box-holder{
+height: 60vh;
+margin-top: 5%;
+display: flex;
+margin-bottom: 10%;
+align-items: flex-end;
+		.hipnoza {
+			
+				width: 60%;
+				margin-left: 8%;
+				height: 80%;
+				margin: 0 auto;
+		}
 
+		.golqma{
+			width:75%;
+			height: 100%;
+		}
 
+		.left{
 
+		}
+
+		.top{
+			margin-top: 10%;
+		}
+}
+
+.with-text{
+height: 100%;
+width: 100%;
+}
 </style>
