867403e3ccf1bc50fababad079a9267863bb4d79
HristoDobrikov <generatorfox@gmail.com>
2021-05-21 13:01:43 +0300

    Страница е започната

--- frontend/src/views/Restaurants.vue
+++ frontend/src/views/Restaurants.vue
@@ -1,3 +1,24 @@
 <template>
-	<h1>Restaurants</h1>
+
+<InfoBox>Вижте най-популярните заведения сред<br> посетителите на сайта</InfoBox>
+<section class="box-holder">
+<restaurant-box></restaurant-box>
+</section>
+
+
+
 </template>
+<script>
+import RestaurantBox from "@/components/home/RestaurantBox";
+import InfoBox from "@/components/others/InfoBox";
+export default {
+   components: { RestaurantBox, InfoBox }
+};
+</script>
+<style lang="scss" scoped>
+
+
+
+
+
+</style>
