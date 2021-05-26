67446c31139330cf3e66eb5f114dd6effd69e59c
Zdravko Hristov <zdravkohristov0@gmail.com>
2021-05-26 16:01:42 +0300

    InfoBox са поправени

--- frontend/src/components/others/InfoBox.vue
+++ frontend/src/components/others/InfoBox.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="info-box">
-        <div class="info-icon">
+        <div class="info-icon" v-if="showIcon">
             <i class="fas fa-info-circle"></i>
         </div>
         <p class="ta-c">
@@ -9,13 +9,30 @@
     </div>
 </template>
 
+<script>
+export default {
+    props: {
+        showIcon: {
+            default: true
+        }
+    }
+};
+</script>
+
 <style lang="scss" scoped>
 .info-box {
-    background-color: var(--clr-other-green);
-    color: white;
+    background-color: #058a5e;
+    color: #e5fff1;
     padding: 30px;
     font-size: 21px;
     position: relative;
+    font-family: "Book Antiqua", Arial;
+
+    p {
+        width: 80%;
+        margin: auto;
+    }
+
     .info-icon {
         position: absolute;
         top: 50%;
