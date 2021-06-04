7da53fd513461f66a44a0a0d87c675e9973b87f5
unknown <denitsa.rashkova@gmail.com>
2021-06-05 01:30:17 +0300

    add files

---
+++ frontend/src/components/restaurants/BigReview.vue
@@ -0,0 +1,112 @@
+<template>
+    <a class="box">
+        <div class="img">
+        <div class="star">
+            4
+        </div>
+        <div class="dark-overlay"></div>
+        </div>
+        <div class="rating">
+            <p>Ресторант<br> "Хипоноза"</p>
+            <div class="icon-holder">
+            <i class="fas fa-long-arrow-alt-up icon-size green"></i>
+            <i class="fas fa-star icon-size green"></i>
+            <h3>50</h3>
+            <i class="fas fa-utensils icon-size red"></i>
+        </div>
+        </div>
+
+    </a>
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/mixins' as *;
+.star {
+    width: 4rem;
+    height: 4rem;
+    background-color: var(--clr-other-green);
+    clip-path: polygon(
+        50% 0%,
+        61% 35%,
+        98% 35%,
+        68% 57%,
+        79% 91%,
+        50% 70%,
+        21% 91%,
+        32% 57%,
+        2% 35%,
+        39% 35%
+    );
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #f3f3f3;
+    transform: translate(-50%, -50%) rotate(-16deg);
+    position: relative;
+    z-index: 1;
+}
+
+.img{
+    position:relative;
+    background-image: url("../../assets/parvomay/hiphop.jpeg");
+    height: 100%;
+    background-size: cover;
+    border: 1px solid #626262;
+
+}
+.dark-overlay {
+    @include overlay(0.3);
+    transition: ease-in-out 0.5s;
+}
+.box {
+    background-size: cover;
+    width: 100%;
+    margin: 0 auto;
+    color: #2b2d28;
+    display: flex;
+    flex-direction: column;
+    &:hover {
+        .dark-overlay {
+            @include overlay(0);
+        }
+        p {
+            text-decoration-color: #dadada54;
+        }
+    }
+}
+
+.rating{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    p{
+        margin-top: 2%;
+        font-size: 1.4rem;
+        transition: ease-in-out 0.5s;
+        text-decoration: underline;
+        text-decoration-color: #dadada00;
+    }
+}
+
+.icon-size{
+    font-size: 22px;
+}
+
+.green{
+    color: var(--clr-other-green);  
+}
+
+.icon-holder{
+    display: flex;
+	align-items: center;
+
+                h3{
+                    font-size: 16px;
+                    color: #d30031;
+                }
+
+                .red{
+                    color: #d30031;
+                }
+}
+</style>
